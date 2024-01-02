10ee1f257ef348972c2cdb1a04f668d8dbcc854e
한 <hih458@naver.com>
2024-01-02 21:07:33 +0900

    [Add] gitignore파일 추가

---
+++ BranchTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BranchTalk
+//
+//  Created by 황인호 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
