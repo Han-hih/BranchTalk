74b2f58f6fefb416c1d3d929508f5fd11c37c1f6
황인호 <109748526+Han-hih@users.noreply.github.com>
2024-01-04 00:51:44 +0900

    [Feat] 카카오톡 로그인 구현, 토큰 값 받아오기 (#3)

--- BranchTalk/AppDelegate.swift
+++ BranchTalk/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         sleep(1)
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
         return true
     }
 
