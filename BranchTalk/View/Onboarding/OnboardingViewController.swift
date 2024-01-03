22d99de575100ca820c377ec3e156631f62d4a9a
한 <hih458@naver.com>
2024-01-03 14:16:59 +0900

    [Design] OnboardingView Design

--- BranchTalk/View/Onboarding/OnboardingViewController.swift
+++ BranchTalk/View/Onboarding/OnboardingViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
-class OnboardingViewController: UIViewController {
-
+final class OnboardingViewController: UIViewController {
+    
+    private let label = {
+        let label = UILabel()
+        label.font = Font.title1Bold()
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.text = "브랜치톡을 사용하면 어디서나\n팀을 모을 수 있습니다"
+        return label
+    }()
+    
+    private let imageView = UIImageView(image: UIImage(named: "onboarding"))
+    
+    private let startButton = {
+        let bt = GreenCustonButton()
+        bt.setTitle("시작하기", for: .normal)
+        return bt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setUI()
     }
 
-
+    private func setUI() {
+        [label, imageView, startButton].forEach {
+            view.addSubview($0)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(93)
+            make.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview().inset(12)
+            make.height.equalTo(imageView.snp.width)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().inset(24)
+            make.height.equalTo(44)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        
+    }
 }
 
