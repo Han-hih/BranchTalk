696ccf5b7c8725d5861720a9c3da71c2d9d84d37
한 <hih458@naver.com>
2024-01-03 13:53:14 +0900

    [Add] 커스텀 버튼 추가

---
+++ BranchTalk/Custom/GreenCustonButton.swift
@@ -0,0 +1,29 @@
+//
+//  GreenCustonButton.swift
+//  BranchTalk
+//
+//  Created by 황인호 on 1/3/24.
+//
+
+import UIKit
+
+class GreenCustonButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButton()
+    }
+    
+    private func setButton() {
+        backgroundColor = Colors.BrandGreen.CutsomColor
+        clipsToBounds = true
+        layer.cornerRadius = 8
+        setTitleColor(Colors.BrandWhite.CutsomColor, for: .normal)
+        titleLabel?.font = Font.title2Bold()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
