7e59a5563305ca90c980c8315fdcaea9ab47b4e7
한 <hih458@naver.com>
2024-01-04 13:53:57 +0900

    [Add] 카카오로그인API 결과값을 디코딩하기 위한 구조체 추가

--- BranchTalk/Model/NetworkModel.swift
+++ BranchTalk/Model/NetworkModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct KakaoResult: Decodable {
+    let userID: Int
+    let email, nickname, vendor, createdAt: String
+    let profileImage, phone: String?
+    let token: Tokens
+ 
+    enum CodingKeys: String, CodingKey {
+        case token
+        case nickname, vendor, createdAt, email, profileImage, phone
+        case userID = "user_id"
+    }
+}
+
+struct Tokens: Decodable {
+    let accessToken, refreshToken: String
+}
