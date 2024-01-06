3306734ef368916faa2be5c4cc9a23dd7ead3268
한 <hih458@naver.com>
2024-01-06 17:37:17 +0900

    [Add] 이메일, 비밀번호, 연락처 유효성 검사

--- BranchTalk/Util/ValidationCheck.swift
+++ BranchTalk/Util/ValidationCheck.swift
@@ -29,15 +29,3 @@ final class ValidationCheck {
         return passwordTest.evaluate(with: input)
     }
 }
-
-//extension String {
-//
-//  public var withHypen: String {
-//    var stringWithHypen: String = self
-//    
-//    stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
-//    stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: -4))
-//    
-//    return stringWithHypen
-//  }
-//}
