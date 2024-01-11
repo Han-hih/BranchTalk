09d4bb151d3d045a93d3e853b34defd10187667a
한 <hih458@naver.com>
2024-01-11 14:57:06 +0900

    [Add] 워크스페이스 존재 유무에 따른 화면 전환, 토큰 상태에 따른 화면 전환

--- BranchTalk/View/Onboarding/LaunchViewController.swift
+++ BranchTalk/View/Onboarding/LaunchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class LaunchViewController: UIViewController {
+final class LaunchViewController: BaseViewController {
     
     private let label = {
         let label = UILabel()
@@ -33,8 +33,9 @@ final class LaunchViewController: UIViewController {
             if !accessToken.isEmpty {
                 NetworkManager.shared.request(type: TokenResponse.self, api: Router.refresh, completion: { [weak self] result in
                     switch result {
-                    case .success:
+                    case .success(let response):
                         print("토큰 재발급 성공")
+                        KeyChain.shared.create(key: "access", token: response.accessToken)
                         // 1. 워크스페이스 유무에 따라 홈뷰 이동
                         self?.goToMainView()
                         
@@ -52,9 +53,24 @@ final class LaunchViewController: UIViewController {
     private func goToMainView() {
         let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
         let sceneDelegate = windowScene?.delegate as? SceneDelegate
-        let vc = HomeEmptyViewController()
-        sceneDelegate?.window?.rootViewController = vc
-        sceneDelegate?.window?.makeKey()
+        NetworkManager.shared.request(type: GetWorkSpaceList.self, api: Router.getWorkSpaceList, completion: { result in
+            switch result {
+            case .success(let response):
+                print(response)
+                if response.count > 0 {
+                    let vc = HomeInitialViewController()
+                    sceneDelegate?.window?.rootViewController = vc
+                } else {
+                    let vc = HomeEmptyViewController()
+                    sceneDelegate?.window?.rootViewController = vc
+                }
+                
+                sceneDelegate?.window?.makeKey()
+            case .failure(let error):
+                print(error)
+                self.goToLoginView()
+            }
+        })
     }
     
     private func goToLoginView() {
@@ -67,7 +83,8 @@ final class LaunchViewController: UIViewController {
     
     
     
-    private func setUI() {
+    override func setUI() {
+        super.setUI()
         [label, imageView].forEach {
             view.addSubview($0)
         }
