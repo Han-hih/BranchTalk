bccfa50161b836fc40d96ceaa68de4ea8f99afa1
한 <hih458@naver.com>
2024-01-12 21:45:44 +0900

    [modify] 화면 전환 함수로 변경 후 수정

--- BranchTalk/View/Onboarding/LaunchViewController.swift
+++ BranchTalk/View/Onboarding/LaunchViewController.swift
@@ -27,61 +27,28 @@ final class LaunchViewController: BaseViewController {
     }
     
     private func checkToken() {
-        guard let accessToken = KeyChain.shared.read(key: "access") else { return }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            if !accessToken.isEmpty {
-                NetworkManager.shared.request(type: TokenResponse.self, api: Router.refresh, completion: { [weak self] result in
-                    switch result {
-                    case .success(let response):
-                        print("토큰 재발급 성공")
-                        KeyChain.shared.create(key: "access", token: response.accessToken)
-                        // 1. 워크스페이스 유무에 따라 홈뷰 이동
-                        self?.goToMainView()
-                        
-                    case .failure(let error):
-                        print(error)
-                        self?.goToLoginView()
-                    }
-                }
-                )} else {
-                    self.goToLoginView()
-                }
-        }
+        guard let accessToken = KeyChain.shared.read(key: "access") else { return ViewMove.shared.goLoginView() }
+        print(accessToken)
+            goToMainView()
     }
     
     private func goToMainView() {
-        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        let sceneDelegate = windowScene?.delegate as? SceneDelegate
-        NetworkManager.shared.request(type: GetWorkSpaceList.self, api: Router.getWorkSpaceList, completion: { result in
+        NetworkManager.shared.request(type: GetWorkSpaceList.self, api: Router.getWorkSpaceList) { result in
+            print(result)
             switch result {
             case .success(let response):
                 print(response)
                 if response.count > 0 {
-                    let vc = HomeInitialViewController()
-                    sceneDelegate?.window?.rootViewController = vc
+                    ViewMove.shared.goHomeInitialView()
                 } else {
-                    let vc = HomeEmptyViewController()
-                    sceneDelegate?.window?.rootViewController = vc
+                    ViewMove.shared.goHomeEmptyView()
                 }
-                
-                sceneDelegate?.window?.makeKey()
             case .failure(let error):
                 print(error)
-                self.goToLoginView()
+                ViewMove.shared.goLoginView()
             }
-        })
+        }
     }
-    
-    private func goToLoginView() {
-        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        let sceneDelegate = windowScene?.delegate as? SceneDelegate
-        let vc = SocialLoginViewController()
-        sceneDelegate?.window?.rootViewController = vc
-        sceneDelegate?.window?.makeKey()
-    }
-    
-    
     
     override func setUI() {
         super.setUI()
