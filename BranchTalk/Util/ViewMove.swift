94da7d8b1b4807ace907565361f34d665cf225b3
한 <hih458@naver.com>
2024-01-12 20:57:29 +0900

    [Add] 조건에 따른 뷰이동 클래스 생성

---
+++ BranchTalk/Util/ViewMove.swift
@@ -0,0 +1,46 @@
+//
+//  ViewMove.swift
+//  BranchTalk
+//
+//  Created by 황인호 on 1/12/24.
+//
+
+import UIKit
+
+final class ViewMove {
+    
+    static let shared = ViewMove()
+    
+    func goHomeInitialView() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        let vc = HomeInitialViewController()
+        sceneDelegate?.window?.rootViewController = vc
+        sceneDelegate?.window?.makeKey()
+    }
+    
+    func goHomeEmptyView() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        let vc = HomeEmptyViewController()
+        sceneDelegate?.window?.rootViewController = vc
+        sceneDelegate?.window?.makeKey()
+    }
+    
+    func goLoginView() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        let vc = OnboardingViewController()
+        sceneDelegate?.window?.rootViewController = vc
+        sceneDelegate?.window?.makeKey()
+    }
+    
+    func goStartWorkSpaceView() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        let vc = StartWorkSpaceViewController()
+        sceneDelegate?.window?.rootViewController = vc
+        sceneDelegate?.window?.makeKey()
+    }
+    
+}
