c971082b85a21c384c0b9c747ca6adaf2ece0417
한 <hih458@naver.com>
2024-01-15 22:15:56 +0900

    [Add] 커스텀 레이블 추가

--- BranchTalk/Custom/CustomLabel.swift
+++ BranchTalk/Custom/CustomLabel.swift
@@ -44,6 +44,23 @@ class CustomTitle2Label: UILabel {
     }
 }
 
+class CustomBodyBoldLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLabel()
+    }
+    
+    private func setLabel() {
+        font = Font.bodyBold()
+        textColor = Colors.BrandBlack.CutsomColor
+        textAlignment = .center
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class CustomBodyLabel: UILabel {
     
     override init(frame: CGRect) {
