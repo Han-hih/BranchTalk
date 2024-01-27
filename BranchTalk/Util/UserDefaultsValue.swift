f1f8e9d6a7b02d740131c9b1880554dc15fe6af1
한 <hih458@naver.com>
2024-01-27 20:23:32 +0900

    [Add] userDefaults 싱글톤 생성

---
+++ BranchTalk/Util/UserDefaultsValue.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults.swift
+//  BranchTalk
+//
+//  Created by 황인호 on 1/27/24.
+//
+
+import Foundation
+
+final class UserDefaultsValue {
+    
+    static let shared = UserDefaultsValue()
+    
+    var workSpaceID = UserDefaults.standard.value(forKey: "workSpaceID")
+    
+}
