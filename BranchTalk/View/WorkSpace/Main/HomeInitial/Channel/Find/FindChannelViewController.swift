5801e0268c9b125b0d04d35409313a0b7e54ca7a
한 <hih458@naver.com>
2024-01-28 17:24:49 +0900

    [Add] 전체 채널리스트 뷰컨트롤러 생성 및 테이블뷰 연결

---
+++ BranchTalk/View/WorkSpace/Main/HomeInitial/Channel/Find/FindChannelViewController.swift
@@ -0,0 +1,58 @@
+//
+//  FindChannelViewController.swift
+//  BranchTalk
+//
+//  Created by 황인호 on 1/28/24.
+//
+
+import UIKit
+
+final class FindChannelViewController: BaseViewController {
+ 
+    private lazy var tableView = {
+        let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        view.register(FindChannelListTableViewCell.self, forCellReuseIdentifier: FindChannelListTableViewCell.identifier)
+        view.rowHeight = 41
+        view.separatorStyle = .none
+        return view
+    }()
+ 
+    override func setNav() {
+        super.setNav()
+        let backButtonItem = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .done, target: self, action: #selector(backButtonTapped))
+        backButtonItem.tintColor = Colors.BrandBlack.CutsomColor
+        self.navigationItem.leftBarButtonItem = backButtonItem
+        navigationItem.title = "채널 탐색"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: Font.navTitle()]
+        self.navigationController?.navigationBar.backgroundColor = Colors.BackgroundSecondary.CutsomColor
+    }
+    
+    override func setUI() {
+        super.setUI()
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.horizontalEdges.verticalEdges.equalToSuperview()
+        }
+    }
+    
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension FindChannelViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FindChannelListTableViewCell.identifier, for: indexPath) as? FindChannelListTableViewCell else { return UITableViewCell() }
+        cell.configure(name: "ddddd")
+        return cell
+    }
+    
+    
+    
+}
