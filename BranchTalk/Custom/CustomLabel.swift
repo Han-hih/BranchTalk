dbcee7b383c6a2fd0bc75f7710f62f755be946e3
한 <hih458@naver.com>
2024-02-15 21:23:38 +0900

    [Add] UILabel Padding 설정하는 Class 추가

--- BranchTalk/Custom/CustomLabel.swift
+++ BranchTalk/Custom/CustomLabel.swift
@@ -95,4 +95,24 @@ class CustomCaptionLabel: UILabel {
     }
 }
 
-
+class BasePaddingLabel: UILabel {
+    
+    private var padding = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
+    
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.padding = padding
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+        
+        return contentSize
+    }
+}
